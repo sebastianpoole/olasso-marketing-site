f5d6beb811614f6921c5cfc8759ac220e913e062
Ace Poole <seb@agogly.com>
2018-03-28 18:01:16 +0800

    Commit with Ant Design

--- src/App.js
+++ src/App.js
@@ -3,20 +3,58 @@ import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import Home from './scenes/Home/index';
 import logo from './logo.svg';
 import './App.css';
- 
+import { Row, Col, Menu, Icon } from 'antd';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
 class App extends Component {
+  state = {
+    current: 'mail',
+  }
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
+  
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <header className="App-header">
-            <Link to="/">
-              <img src={logo} className="App-logo" alt="logo" />
-            </Link>
-              <h1 className="App-title">Olasso</h1>
-          </header>
+        <Row>
+          <Col span={12}>
+            <header className="App-header">
+              <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="mail">
+          <Icon type="mail" />Navigation One
+        </Menu.Item>
+        <Menu.Item key="app" disabled>
+          <Icon type="appstore" />Navigation Two
+        </Menu.Item>
+        <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
+          <MenuItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <Menu.Item key="alipay">
+          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+        </Menu.Item>
+      </Menu>
+            </header>
+          </Col>
+        </Row>
           <Switch>
-            <Route exact path="/" component={Home} />
             <Route exact path="/" component={Home} />
           </Switch>
         </div>
